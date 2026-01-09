2080af9283d1bed8e5043de1f977be814b65d0fc
akovachev-next <angelkovachev2015@gmail.com>
2026-01-09 16:46:34 +0200

    first commit

---
+++ src/main/java/com/exercise/sevriceconfiguration/SevriceconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.sevriceconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevriceconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevriceconfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/sevriceconfiguration/SevriceconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.sevriceconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevriceconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
